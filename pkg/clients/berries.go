f31596bbde05ef2df6dee44363815ca38e5e7de6
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-05-07 17:20:47 -0400

    Add GetBerryByName

--- pkg/clients/berries.go
+++ pkg/clients/berries.go
@@ -10,13 +10,13 @@ import (
 )
 
 type IBerries interface {
-	GetBerryById(id int) (types.Berry, error)
-	GetBerryByName(name string) (types.Berry, error)
+	GetBerryById(id int) (*types.Berry, error)
+	GetBerryByName(name string) (*types.Berry, error)
 }
 
 type Berries struct{}
 
-func (b Berries) GetBerryById(id int) (types.Berry, error) {
+func (b Berries) GetBerryById(id int) (*types.Berry, error) {
 	var berry types.Berry
 
 	parsedId := strconv.Itoa(id)
@@ -25,17 +25,39 @@ func (b Berries) GetBerryById(id int) (types.Berry, error) {
 
 	response, err := http.Get(url)
 	if err != nil {
-		return berry, types.ErrFetch
+		return &berry, types.ErrFetch
 	}
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return berry, types.ErrBody
+		return &berry, types.ErrBody
 	}
 
 	if err = json.Unmarshal(body, &berry); err != nil {
-		return berry, types.ErrUnmarshal
+		return &berry, types.ErrUnmarshal
 	}
 
-	return berry, nil
+	return &berry, nil
+}
+
+func (b Berries) GetBerryByName(name string) (*types.Berry, error) {
+	var berry types.Berry
+
+	url := "https://pokeapi.co/api/v2/berry/" + name
+
+	response, err := http.Get(url)
+	if err != nil {
+		return &berry, types.ErrFetch
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return &berry, types.ErrBody
+	}
+
+	if err = json.Unmarshal(body, &berry); err != nil {
+		return &berry, types.ErrUnmarshal
+	}
+
+	return &berry, nil
 }
--- pkg/clients/berries_test.go
+++ pkg/clients/berries_test.go
@@ -8,68 +8,70 @@ import (
 	"github.com/jdplumst/pokeapigo/pkg/types"
 )
 
+var (
+	want = &types.Berry{
+		Id:               1,
+		Name:             "cheri",
+		GrowthTime:       3,
+		MaxHarvest:       5,
+		NaturalGiftPower: 60,
+		Size:             20,
+		Smoothness:       25,
+		SoilDryness:      15,
+		Firmness: types.NamedApiResource{
+			Name: "soft",
+			Url:  "https://pokeapi.co/api/v2/berry-firmness/2/",
+		},
+		Flavors: []types.BerryFlavorMap{
+			{Potency: 10,
+				Flavor: types.NamedApiResource{
+					Name: "spicy",
+					Url:  "https://pokeapi.co/api/v2/berry-flavor/1/",
+				},
+			},
+			{Potency: 0,
+				Flavor: types.NamedApiResource{
+					Name: "dry",
+					Url:  "https://pokeapi.co/api/v2/berry-flavor/2/",
+				},
+			},
+			{Potency: 0,
+				Flavor: types.NamedApiResource{
+					Name: "sweet",
+					Url:  "https://pokeapi.co/api/v2/berry-flavor/3/",
+				},
+			},
+			{Potency: 0,
+				Flavor: types.NamedApiResource{
+					Name: "bitter",
+					Url:  "https://pokeapi.co/api/v2/berry-flavor/4/",
+				},
+			},
+			{Potency: 0,
+				Flavor: types.NamedApiResource{
+					Name: "sour",
+					Url:  "https://pokeapi.co/api/v2/berry-flavor/5/",
+				},
+			},
+		},
+		Item: types.NamedApiResource{
+			Name: "cheri-berry",
+			Url:  "https://pokeapi.co/api/v2/item/126/",
+		},
+		NaturalGiftType: types.NamedApiResource{
+			Name: "fire",
+			Url:  "https://pokeapi.co/api/v2/type/10/",
+		},
+	}
+)
+
 func TestGetBerryById(t *testing.T) {
 	berryClient := clients.Berries{}
 
 	t.Run("valid id", func(t *testing.T) {
 		got, err := berryClient.GetBerryById(1)
 		if err != nil {
-			t.Error("did not return a berry", err)
-		}
-
-		want := types.Berry{
-			Id:               1,
-			Name:             "cheri",
-			GrowthTime:       3,
-			MaxHarvest:       5,
-			NaturalGiftPower: 60,
-			Size:             20,
-			Smoothness:       25,
-			SoilDryness:      15,
-			Firmness: types.NamedApiResource{
-				Name: "soft",
-				Url:  "https://pokeapi.co/api/v2/berry-firmness/2/",
-			},
-			Flavors: []types.BerryFlavorMap{
-				{Potency: 10,
-					Flavor: types.NamedApiResource{
-						Name: "spicy",
-						Url:  "https://pokeapi.co/api/v2/berry-flavor/1/",
-					},
-				},
-				{Potency: 0,
-					Flavor: types.NamedApiResource{
-						Name: "dry",
-						Url:  "https://pokeapi.co/api/v2/berry-flavor/2/",
-					},
-				},
-				{Potency: 0,
-					Flavor: types.NamedApiResource{
-						Name: "sweet",
-						Url:  "https://pokeapi.co/api/v2/berry-flavor/3/",
-					},
-				},
-				{Potency: 0,
-					Flavor: types.NamedApiResource{
-						Name: "bitter",
-						Url:  "https://pokeapi.co/api/v2/berry-flavor/4/",
-					},
-				},
-				{Potency: 0,
-					Flavor: types.NamedApiResource{
-						Name: "sour",
-						Url:  "https://pokeapi.co/api/v2/berry-flavor/5/",
-					},
-				},
-			},
-			Item: types.NamedApiResource{
-				Name: "cheri-berry",
-				Url:  "https://pokeapi.co/api/v2/item/126/",
-			},
-			NaturalGiftType: types.NamedApiResource{
-				Name: "fire",
-				Url:  "https://pokeapi.co/api/v2/type/10/",
-			},
+			t.Errorf("did not return a berry, %v", err)
 		}
 
 		if !reflect.DeepEqual(got, want) {
@@ -79,6 +81,35 @@ func TestGetBerryById(t *testing.T) {
 
 	t.Run("invalid id", func(t *testing.T) {
 		_, err := berryClient.GetBerryById(100)
+
+		switch err {
+		case types.ErrFetch, types.ErrBody, types.ErrUnmarshal:
+			return
+		case nil:
+			t.Error("got a berry when expected an error:", err)
+		default:
+			t.Error("got an unexpected error:", err)
+
+		}
+	})
+}
+
+func TestGerBerryByName(t *testing.T) {
+	berryClient := clients.Berries{}
+
+	t.Run("valid name", func(t *testing.T) {
+		got, err := berryClient.GetBerryByName("cheri")
+		if err != nil {
+			t.Errorf("did not return a berry, %v", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %+v\nwant %+v\n", got, want)
+		}
+	})
+
+	t.Run("invalid name", func(t *testing.T) {
+		_, err := berryClient.GetBerryByName("invalid")
 
 		switch err {
 		case types.ErrFetch, types.ErrBody, types.ErrUnmarshal:
